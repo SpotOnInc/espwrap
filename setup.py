fb749601690a297dd5399dad1e96c242b29ae12d
Jim Battin <jim@spoton.com>
2016-03-23 13:17:55 -0500

    Set version in __init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from __future__ import print_function, division, unicode_literals
 
+import re
+import ast
+
 from setuptools import setup
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('espwrap/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 setup(
     name='espwrap',
-    version='1.0.0',
+    version=version,
     description='A light wrapper around email service providers',
     url='https://github.com/SpotOnInc/espwrap',
     license='MIT',
