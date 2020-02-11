10fb7cd7ed3ab54d725f2c5baa7eedfbb4703ba7
Francisco Pantoja <fcopantoja@gmail.com>
2020-02-11 09:25:49 -0600

    Fix email sending for multiple recipients

--- espwrap/adaptors/sendgrid_v3.py
+++ espwrap/adaptors/sendgrid_v3.py
@@ -36,11 +36,11 @@ class SendGridMassEmail(MassEmail):
 
         self.delimiters = ('-', '-')
 
-        
+
     def set_variable_delimiters(self, start='-', end='-'):
         self.delimiters = (start, end)
 
-        
+
     def get_variable_delimiters(self, as_dict=False):
         if as_dict:
             return {
@@ -49,7 +49,7 @@ class SendGridMassEmail(MassEmail):
             }
         return self.delimiters
 
-    
+
     def add_tags(self, *tags):
         if len(tags) > 10:
             raise Exception('Too many tags, SendGrid limits to 10 per email')
@@ -61,109 +61,108 @@ class SendGridMassEmail(MassEmail):
 
     def message_constructor(self, to_send):
         message = Mail()
-        
+        to_emails = []
+
+        # Content (Text)
+        if self.body[MIMETYPE_TEXT]:
+            content = Content(
+                MimeType.text,
+                self.body[MIMETYPE_TEXT]
+            )
+            message.content = content
+
+        # Content (Html)
+        if self.body[MIMETYPE_HTML]:
+            content = Content(
+                MimeType.html,
+                self.body[MIMETYPE_HTML]
+            )
+            message.content = content
+
+        message.subject = Subject(self.subject)
+
+        # Category
+        if self.tags:
+            for tag in self.tags:
+                message.category = Category(tag)
+
+        # Reply
+        if self.reply_to_addr:
+            message.reply_to = ReplyTo(self.reply_to_addr)
+
+        # Send At
+        if self.send_at:
+            message.send_at = SendAt(self.send_at)
+
+        # IP Pool
+        if self.ip_pool:
+            message.ip_pool_name = IpPoolName(self.ip_pool)
+
+        # CC
+        if self.cc_list:
+            list_cc = []
+            for email in self.cc_list:
+                list_cc.append(Cc(email))
+            message.cc = list_cc
+
+        # BCC
+        if self.bcc_list:
+            list_bcc = []
+            for email in self.bcc_list:
+                list_bcc.append(Bcc(email))
+            message.bcc = list_bcc
+
+        # Attachment
+        if self.attachments:
+            for file_name, file_path_or_string in self.attachments.iteritems():
+                attachment = Attachment()
+                encoded = base64.b64encode(str(file_path_or_string))
+                attachment.file_content = FileContent(encoded)
+                attachment.file_name = FileName(file_name)
+            message.attachment = attachment
+
+        # Webhook Data
+        if self.webhook_data:
+            for key, val in self.webhook_data.items():
+                message.custom_arg = CustomArg(key, val)
+
+        # Metadata
+        if hasattr(self, 'metadata'):
+            for key, val in self.metadata.items():
+                message.custom_arg = CustomArg(key, val)
+
+        # Template Name
+        if self.template_name:
+            message.custom_arg = CustomArg('template_name', self.template_name)
+
+        # Tracking
+        tracking_settings = TrackingSettings()
+        # Opens
+        if self.track_opens:
+            tracking_settings.open_tracking = OpenTracking(
+                self.track_opens,
+                OpenTrackingSubstitutionTag("open_tracking")
+            )
+        # Clicks
+        if self.track_clicks:
+            tracking_settings.click_tracking = ClickTracking(
+                self.track_clicks,
+                self.track_clicks
+            )
+        message.tracking_settings = tracking_settings
+        message.from_email = From(self.from_addr)
+
         for subgrp in to_send:
-            #Reply
-            if self.reply_to_addr:
-                message.reply_to = ReplyTo(self.reply_to_addr)
+            substitutions = subgrp[0]['merge_vars']
+            substitutions = {'{1}{0}{2}'.format(x, *self.delimiters): substitutions[x] for x in substitutions}
+            to_emails.append(To(subgrp[0]['email'], subgrp[0]['name'], substitutions=substitutions))
 
-            #Send At
-            if self.send_at:
-                message.send_at = SendAt(self.send_at)
+        message.add_to(to_emails, is_multiple=True)
 
-            #Category
-            if self.tags:
-                for tag in self.tags:
-                    message.category = Category(tag)
-
-            #IP Pool
-            if self.ip_pool:
-                message.ip_pool_name = IpPoolName(self.ip_pool)
-
-            #CC
-            if self.cc_list:
-                list_cc = []
-                for email in self.cc_list:
-                    list_cc.append(Cc(email))
-                message.cc = list_cc
-
-            #BCC
-            if self.bcc_list:
-                list_bcc = []
-                for email in self.bcc_list:
-                    list_bcc.append(Bcc(email))
-                message.bcc = list_bcc
-
-            #Content (Text)
-            if self.body[MIMETYPE_TEXT]:
-                content = Content(
-                    MimeType.text,
-                    self.body[MIMETYPE_TEXT]
-                )
-                message.content = content
-
-            #Content (Html)
-            if self.body[MIMETYPE_HTML]:
-                content = Content(
-                    MimeType.html,
-                    self.body[MIMETYPE_HTML]
-                )
-                message.content = content
-
-            #Attachment
-            if self.attachments:
-                for file_name, file_path_or_string in self.attachments.iteritems():
-                    attachment = Attachment()
-                    encoded = base64.b64encode(str(file_path_or_string))
-                    attachment.file_content = FileContent(encoded)
-                    attachment.file_name = FileName(file_name)
-                message.attachment = attachment
-
-            #Webhook Data
-            if self.webhook_data:
-                for key, val in self.webhook_data.items():
-                    message.custom_arg = CustomArg(key, val)
-
-            #Metadata
-            if hasattr(self, 'metadata'):
-                for key, val in self.metadata.items():
-                    message.custom_arg = CustomArg(key, val)                       
-
-            #Template Name
-            if self.template_name:
-                message.custom_arg = CustomArg('template_name', self.template_name)
-
-            #Tracking
-            tracking_settings = TrackingSettings()            
-            #Opens
-            if self.track_opens:
-                tracking_settings.open_tracking = OpenTracking(
-                    self.track_opens,
-                    OpenTrackingSubstitutionTag("open_tracking")
-                )
-            #Clicks
-            if self.track_clicks:
-                tracking_settings.click_tracking = ClickTracking(
-                    self.track_clicks,
-                    self.track_clicks
-                )
-            message.tracking_settings = tracking_settings
-
-            #Set personalization fields
-            for p, recip in enumerate(subgrp):
-                message.from_email = From(self.from_addr)
-                message.to = To(recip['email'], recip['name'], p=p)
-                message.subject = Subject(self.subject)
-
-                #Global Subs
-                for key, val in self.global_merge_vars.items():
-                    new_key = '{1}{0}{2}'.format(key, *self.delimiters)
-                    message.substitution =  Substitution(new_key, val, p=p)
-
-                #Substitutions
-                for key, val in recip['merge_vars'].items():
-                    new_key = '{1}{0}{2}'.format(key, *self.delimiters)
-                    message.substitution = Substitution(new_key, val, p=p)
+        # Global Subs
+        for key, val in self.global_merge_vars.items():
+            new_key = '{1}{0}{2}'.format(key, *self.delimiters)
+            message.substitution = Substitution(new_key, val)
 
         return message
 
@@ -173,12 +172,12 @@ class SendGridMassEmail(MassEmail):
         responses=[]
 
         self.validate()
-        
+
         grouped_recipients = batch(list(self.recipients), self.partition)
 
         #DEBUG
         print (self.recipients)
-        
+
         for grp in grouped_recipients:
             to_send = breakdown_recipients(grp)
 
@@ -194,20 +193,22 @@ class SendGridMassEmail(MassEmail):
                 custom_args_kb = 0.001 * float(custom_args_size)
                 if custom_args_kb > 10:
                     raise Exception('attempted to send {} KB custom_args, not to exceed 10 KB.'.format(custom_args_kb))
-                
+
             #do not send if number of recipients >1000 (sendgrid rule)
-            num_recips = len(message_dict['personalizations'])
-            if num_recips > 1000:
-                raise Exception('attempted to send to {} email addresses, not to exceed 1000 addresses.'.format(num_recips))
-            
+            #num_recips = len(message_dict['personalizations'])
+            #if num_recips > 1000:
+            #    raise Exception('attempted to send to {} email addresses, not to exceed 1000 addresses.'.format(num_recips))
+
             """
             send message and append response from this grp to list of returned responses for all grouped_recipients
             """
             try:
+                print(message)
+                #del message['personalizations'][0]['substitutions']
                 response = self.client.send(message)
                 responses.append(response)
             except Exception:
                 logger.exception('')
-            
+
         #return list of all responses for each grp in grouped_recipients
         return responses
